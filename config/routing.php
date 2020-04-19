fd9b2b3301ab75d555f36274d1ce68e15711613d
Anargya Widyadhana <anargyawidyadhana@gmail.com>
2020-04-19 16:34:06 +0700

    update model article dan hapus routing modul

--- config/routing.php
+++ config/routing.php
@@ -1,31 +1,67 @@
 <?php
-$di['router'] = function () use (
-    $default_module,
-    $modules,
-    $di,
-    $config
-) {
+$di['router'] = function () use ($default_module, $modules, $di, $config) {
+ 
     $router = new \Phalcon\Mvc\Router(false);
     $router->clear();
-    $moduleRouting = __DIR__ . '/../apps/' . ucfirst($default_module) . '/Config/routing.php';
-    if (file_exists($moduleRouting) && is_file($moduleRouting)) {
-        $router = include $moduleRouting;
-    } else {
-        $router->add('#^/(|/)$#', array(
-            'module' => $default_module,
-            'controller' => 'index',
-            'action' => 'index',
+    
+    // Default routing
+    $router->add('/', array(
+        'module' => $default_module,
+        'controller' => 'index',
+        'action' => 'index'
+    ));
+ 
+    // Untuk controller tertentu di dalam default module (frontend)
+    $router->add('/:controller/:params', array(
+        'module' => $default_module,
+        'controller' => 1, // Artinya parameter pertama (/:controller)
+        'action' => 'index', // Default action diarahkan ke index
+        'params' => 2, // artinya parameter kedua (/:params)
+    ));
+ 
+    // Untuk controller dan action tertentu di dalam default module (frontend)
+    $router->add('/:controller/:action/:params', array(
+        'module' => $default_module,
+        'controller' => 1,
+        'action' => 2,
+        'params' => 3
+    ));
+ 
+ 
+    // Untuk routing module-module yang lain
+    foreach ($modules as $moduleName => $module) {
+        
+        // Jika nama module adalah frontend, maka tidak perlu ditambahkan
+        if($moduleName == $default_module)
+            continue;
+ 
+        // Untuk index masing-masing module, contoh: /backoffice/, /api/, /core/
+        $router->add('/'. $moduleName .'/:params', array(
+            'module' => $moduleName,
+            'controller' => 'index', // Default controller diarahkan ke IndexController
+            'action' => 'index',  // Default action diarahkan ke index di dalam IndexController
+            'params' => 1
         ));
-        $router->add('#^/([a-zA-Z0-9\_]+)[/]{0,1}$#', array(
-            'module' => $default_module,
-            'controller' => 1,
+ 
+        // Untuk controller tertentu di dalam module
+        $router->add('/'. $moduleName .'/:controller/:params', array(
+            'module' => $moduleName,
+            'controller' => 1, // Artinya parameter pertama (/:controller)
+            'action' => 'index', // Default action diarahkan ke index
+            'params' => 2, // artinya parameter kedua (/:params)
         ));
-        $router->add('#^/{0,1}([a-zA-Z0-9\_]+)/([a-zA-Z0-9\_]+)(/.*)*$#', array(
-            'module' => $default_module,
+ 
+        // Untuk controller dan action tertentu di dalam module
+        $router->add('/'. $moduleName .'/:controller/:action/:params', array(
+            'module' => $moduleName,
             'controller' => 1,
             'action' => 2,
-            'params' => 3,
+            'params' => 3
         ));
     }
+ 
+    // Dapat diaktifkan jika ingin melihat hasil penambahan routing
+    var_dump($router);
+ 
     return $router;
 };
