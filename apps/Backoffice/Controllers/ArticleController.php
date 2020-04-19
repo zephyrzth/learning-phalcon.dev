1e3dc2c3ca82bdb79d8f9617841fbee4a111d0dc
Anargya Widyadhana <anargyawidyadhana@gmail.com>
2020-04-19 18:32:07 +0700

    create data via controller

--- apps/Backoffice/Controllers/ArticleController.php
+++ apps/Backoffice/Controllers/ArticleController.php
@@ -24,4 +24,23 @@ class ArticleController extends BaseController
         // Sisipkan variabel hasil query ke dalam view
         $this->view->articles = $articles;
     }
+
+    public function createAction()
+    {
+        $this->view->disable();
+ 
+        $article_manager = $this->getDI()->get('core_article_manager');
+        
+        try {
+            $article = $article_manager->create([
+                'article_short_title' => 'Test article short title 5',
+                'article_long_title' => 'Test article long title 5',
+                'article_description' => 'Test article description 5',
+                'article_slug' => 'test-article-short-title-5'
+            ]);
+            echo $article->getArticleShortTitle(), " was created.";
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 }
--- apps/Core/Managers/ArticleManager.php
+++ apps/Core/Managers/ArticleManager.php
@@ -6,6 +6,25 @@ use App\Core\Models\Article;
  
 class ArticleManager extends BaseManager
 {
+    public function create($data)
+    {
+        $article = new Article();
+        $article->setArticleShortTitle($data['article_short_title']);
+        $article->setArticleLongTitle($data['article_long_title']);
+        $article->setArticleDescription($data['article_description']);
+        $article->setArticleSlug($data['article_slug']);
+        $article->setIsPublished(0);
+        $article->setCreatedAt(new \Phalcon\Db\RawValue('NOW()'));
+        
+        if (false === $article->create()) {
+            foreach ($article->getMessages() as $message) {
+                $error[] = (string) $message;
+            }
+            throw new \Exception(json_encode($error));
+        }
+        return $article;
+    }
+
     public function find($parameters = null)
     {
         return Article::find($parameters);
