f1cc996dcc923f61891af716e1e7cb2d869ad2ea
Anargya Widyadhana <anargyawidyadhana@gmail.com>
2020-04-19 17:42:54 +0700

    sort article by desc created time

--- apps/Backoffice/Controllers/ArticleController.php
+++ apps/Backoffice/Controllers/ArticleController.php
@@ -8,11 +8,20 @@ class ArticleController extends BaseController
     {
         return $this->dispatcher->forward(['action' => 'list']);
     }
+    
     public function listAction()
     {
+        // Dapatkan objek dari dependency containrr
         $article_manager = $this->getDI()->get(
             'core_article_manager'
         );
-        $this->view->articles = $article_manager->find();
+ 
+        // Lakukan query dengan menambahkan sorting
+        $articles = $article_manager->find([
+            'order' => 'created_at DESC'
+        ]);
+ 
+        // Sisipkan variabel hasil query ke dalam view
+        $this->view->articles = $articles;
     }
 }
